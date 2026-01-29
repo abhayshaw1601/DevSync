edb11eae22f86636cf9ce381c06ad8250fd340a8
Abhay Shaw <abhayshaw1601@gmail.com>
2026-01-29 23:23:59 +0530

    Remove standalone output for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'standalone', // For Docker deployment
 };
 
 export default nextConfig;
